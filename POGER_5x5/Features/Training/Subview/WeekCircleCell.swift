34facda2288b13511c3b5837d36a335767027188
textobey <dogsan_e@naver.com>
2023-02-12 22:38:57 +0900

    운동 화면 UX 구상 중, 기본 형상 작업 진행

---
+++ POGER_5x5/Features/Training/Subview/WeekCircleCell.swift
@@ -0,0 +1,57 @@
+//
+//  WeekCircleCell.swift
+//  POGER_5x5
+//
+//  Created by 이서준 on 2023/02/11.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class WeekCircleCell: UICollectionViewCell {
+    
+    static let identifier = String(describing: WeekCircleCell.self)
+    
+    var disposeBag = DisposeBag()
+    
+    private enum Const {
+        static let circleSize = CGSize(width: 80, height: 80)
+    }
+    
+    let circleView = UIView().then {
+        $0.backgroundColor = .tertiarySystemBackground
+        $0.layer.cornerRadius = Const.circleSize.height / 2
+    }
+    
+    let weekLabel = UILabel().then {
+        $0.text = "WEEK 1"
+        $0.textColor = .label
+        $0.textAlignment = .center
+        $0.numberOfLines = 1
+        $0.font = .preferredFont(forTextStyle: .body)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        contentView.addSubview(circleView)
+        circleView.snp.makeConstraints {
+            $0.directionalEdges.equalToSuperview()
+            $0.size.equalTo(Const.circleSize)
+        }
+        
+        circleView.addSubview(weekLabel)
+        weekLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
--- POGER_5x5/Features/Training/TrainingViewController.swift
+++ POGER_5x5/Features/Training/TrainingViewController.swift
@@ -23,6 +23,27 @@ class TrainingViewController: UIViewController {
         self.present(navigationController, animated: true)
     }()
     
+    //TODO: 마지막 Cell 위치까지 스크롤 되지 않는 이슈 해결
+    //TODO: WeekCircleCell도 TrainingListCell에 포함시키기
+    //TODO: UI 구상하기 When isSelected
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout).then {
+        $0.backgroundColor = .clear
+        $0.isScrollEnabled = true
+        $0.isPagingEnabled = false
+        $0.showsVerticalScrollIndicator = false
+        $0.showsHorizontalScrollIndicator = false
+        $0.contentInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 0)
+        $0.register(WeekCircleCell.self, forCellWithReuseIdentifier: WeekCircleCell.identifier)
+    }
+    
+    private let collectionViewFlowLayout = UICollectionViewFlowLayout().then {
+        $0.scrollDirection = .horizontal
+        $0.minimumLineSpacing = 16
+        $0.minimumInteritemSpacing = 0
+        $0.estimatedItemSize = CGSize(width: 80, height: 80)
+        $0.itemSize = UICollectionViewFlowLayout.automaticSize
+    }
+    
     let tableView = DynamicHeightTableView().then {
         $0.estimatedRowHeight = 44
         $0.rowHeight = UITableView.automaticDimension
@@ -39,13 +60,23 @@ class TrainingViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         //MARK: - Start Guide Flow 잠시 비활성화
-        //_ = takeOncePush
+        //TODO: 초기 실행시, TrainingViewController 잠시 표시된 이후에 present 되는 문제 해결
+        _ = takeOncePush
     }
     
     private func setupLayout() {
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(80)
+        }
+        
         view.addSubview(tableView)
         tableView.snp.makeConstraints {
-            $0.directionalEdges.equalToSuperview()
+            $0.top.equalTo(collectionView.snp.bottom).offset(32)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
         }
     }
     
@@ -57,5 +88,18 @@ class TrainingViewController: UIViewController {
             ) { row, element, cell in
                 cell.configureCell(day: element)
             }.disposed(by: disposeBag)
+        
+        Observable.just(
+            Array(repeating: "WEEK ", count: 12)
+                .enumerated()
+                .map { index, element in
+                    element + "\(index + 1)"
+                }
+        ).bind(to: collectionView.rx.items(
+            cellIdentifier: WeekCircleCell.identifier,
+            cellType: WeekCircleCell.self
+        )) { row, element, cell in
+            cell.weekLabel.text = element
+        }.disposed(by: disposeBag)
     }
 }
